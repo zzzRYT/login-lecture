9311b18f124bd9669a12bf56469842855ff299c4
zzzRYT <jinjinstar3@gmail.com>
2023-06-23 17:36:26 +0900

    프론트엔드 javascript파일 연결

--- app/app.js
+++ app/app.js
@@ -9,6 +9,9 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/', home);
+//현재위치의/src/public 안의 폴더를 정적 경로로 추가한다.
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use('/', home); //미들웨어를 등록 
 
 module.exports = app;
