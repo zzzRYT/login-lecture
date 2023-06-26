72da041c1054edc0122341561b82adac7fb14376
zzzRYT <jinjinstar3@gmail.com>
2023-06-26 17:20:03 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,37 +5,21 @@ const User = require('../../models/User');
 
 const output = {
     home: (req, res) => {
-        res.render('home/index')
+        res.render('home/index');
     },
     login: (req, res) => {
-        res.render('home/login')
-    }
+        res.render('home/login');
+    },
+    register: (req, res)=> {
+        res.render('home/register');
+    },
 };
-
 
 const process = {
     login: (req, res) => {
         const user = new User(req.body);
         const response = user.login();
-        // console.log(response);
         return res.json(response);
-        // const id = req.body.id;
-        // const pass = req.body.pass;
-        // const users = UserStorage.getUsers("id", "pass");
-
-        // if(users.id.includes(id)) {
-        //     const idx = users.id.indexOf(id);
-        //     if (users.pass[idx] === pass) {
-        //         return res.json({
-        //             success: true,  //로그인이 성공하게 되면 success와 msg를 json으로 묶어서 res(front단 으로)응답을 하게 된다.
-        //             msg: '로그인 성공!',
-        //         });
-        //     }
-        // }
-        // return res.json( {  //동일하게 json으로 묶어서 res로 응답해줌
-        //     success: false,
-        //     msg: '로그인 실패!',
-        // });
     },
 };
 
